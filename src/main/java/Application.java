93d93544e6ed77a5e35220e10c8eb94212ba00fa
Nandy <nilanila2k@yahoo.co.in>
2021-05-21 21:41:22 +0100

    new5

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("running main");
         System.out.println("new line 1");
+        System.out.println("new line 2");
     }
 }
